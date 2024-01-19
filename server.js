f82fd50423a04e2f0083fe4e4f01cf37da21eb33
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-01-19 12:20:31 -0500

    added static files and set up cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express  = require('express')
 const path = require('path')
 const fs = require('fs')
+const cors=  require('cors')
 
 const PORT = 3333
 
@@ -18,9 +19,17 @@ async function saveUserData(userArr) {
     console.log('User Data uploaded')
 }
 
-app.get('/', (requestObj, responseObj) => {
-    responseObj.sendFile(path.join(__dirname, './public/index.html'))
-})
+app.use(express.static('./public'))
+
+app.use(cors())
+// app.get('/', (requestObj, responseObj) => {
+//     responseObj.sendFile(path.join(__dirname, './public/index.html'))
+// })
+
+// app.get('/css/style.css', (requestObj, responseObj) => {
+//     responseObj.sendFile(path.join(__dirname, './public/css/style.css'))
+// })
+
 
 // Opening up the middleware channel to allow json to be sent through from the client
 app.use(express.json())
