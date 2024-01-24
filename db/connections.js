8bae26a439f5e3d6851699a9417ea7652f3ca3bc
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-01-24 12:54:51 -0500

    ready for deployment

--- db/connections.js
+++ db/connections.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2')
+require('dotenv').config()
+
+const is_prod = process.env.NODE_ENV === 'production'
+
 
 //create connection to mysql database
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-  
-    database: 'msql_first_day_db'
+    host: process.env.DP_HOST_URL,
+    user: DB_USER,
+    password: is_prod ? process.env.DB_PASSWORD : '',
+    database: DB_DATABASE_NAME
 })
 
 
