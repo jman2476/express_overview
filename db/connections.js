dd13b6ba2f5ffc36ead0dab5c4c06386c7e76812
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-01-25 10:28:59 -0500

    fix stuff

--- db/connections.js
+++ db/connections.js
@@ -1,15 +1,13 @@
 const mysql = require('mysql2')
 require('dotenv').config()
 
-const is_prod = process.env.NODE_ENV === 'production'
-
 
 //create connection to mysql database
 const db = mysql.createConnection({
     host: process.env.DP_HOST_URL,
-    user: DB_USER,
-    password: is_prod ? process.env.DB_PASSWORD : '',
-    database: DB_DATABASE_NAME
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE_NAME
 })
 
 
